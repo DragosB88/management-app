4eacfd4366f63ba62487bf96c5666d02b43653fd
Dragos <s.dragosbodea@gmail.com>
2020-07-29 15:26:21 +0300

    added delete functionality

--- src/app/components/company-details/company-details.component.ts
+++ src/app/components/company-details/company-details.component.ts
@@ -7,11 +7,20 @@ import { ManagementEmployeesService } from '../../services/management-employees.
   styleUrls: ['./company-details.component.scss'],
 })
 export class CompanyDetailsComponent implements OnInit {
-  employeesData: {};
+  employeesData;
   constructor(ManagementEmployeesService: ManagementEmployeesService) {
     this.employeesData = ManagementEmployeesService.getEmployees();
     console.log('Data ', this.employeesData);
   }
-
+  deletedEmployee(ev) {
+    if (ev.deleted) {
+      console.log('the user with the id ', ev.id, ' has been deleted.');
+      this.employeesData = this.employeesData.filter((obj) => {
+        console.log('obj.id ', obj.Id, 'ev.id ', ev.id);
+        return obj.Id !== ev.id;
+      });
+      console.log('new array ', this.employeesData);
+    }
+  }
   ngOnInit(): void {}
 }
--- src/app/components/employee-details/employee-details.component.ts
+++ src/app/components/employee-details/employee-details.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrls: ['./employee-details.component.css'],
+  styleUrls: ['./employee-details.component.scss'],
 })
 export class EmployeeDetailsComponent implements OnInit {
+  @Input() employeeData;
+  @Output() deleteEntry = new EventEmitter();
+  isDeleted: boolean = false;
   constructor() {}
+  deleteEmployee() {
+    this.isDeleted = true;
 
+    let code = {
+      deleted: this.isDeleted,
+      id: this.employeeData.Id,
+    };
+    console.log('CODE: ', code);
+    this.deleteEntry.emit(code);
+  }
   ngOnInit(): void {}
 }
